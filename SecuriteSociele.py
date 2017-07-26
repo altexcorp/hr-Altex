8b4222148087f5accbaf66754783b12c84238189
ilyashakim <hakimguettaoui@gmail.com>
2017-07-26 16:17:30 +0100

    add fonction to calcul IRG and social Security

---
+++ SecuriteSociele.py
@@ -0,0 +1,7 @@
+
+class CalculSecuriter():
+    def SecuriteSociele(self, Basecotisable):
+        SecuriteSociale = 0.09
+        SalaireImposable = Basecotisable - (Basecotisable * SecuriteSociale)
+
+        return SalaireImposable
