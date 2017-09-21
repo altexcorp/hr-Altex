4556fdc31265aa0f1f9634f02c05ee28b02772c0
ilyashakim <hakimguettaoui@gmail.com>
2017-09-21 13:38:43 +0100

    add new

--- models/altex_payroll.py
+++ models/altex_payroll.py
@@ -5,8 +5,8 @@ from openerp import models , fields , api , _
 class Hr_Contract(models.Model):
 
     _inherit = 'hr.contract'
-    bonus = fields.Float(string='Prime' ,default=0.0)
-    transport = fields.Float(string='Transport' ,default=0.0)
+    #bonus = fields.Float(string='Prime' ,default=0.0)
+    #transport = fields.Float(string='Transport' ,default=0.0)
     #code = fields.char('Code', size=52, required=True, help="The code that can be used in the salary rules")
 
 class Hr_Payslip(models.Model):
@@ -15,6 +15,7 @@ class Hr_Payslip(models.Model):
     date_today = fields.Date('Date D',default=fields.Datetime.now)
     #contract_id = fields.Many2one('hr.contract', 'Contract to payslip')
     #transfer_now = fields.Many2one('hr.payslip.line','Transfer Now')
-
-
-
+    prim_sud = fields.Float(string='Prime de Sud' ,default=0.0)
+    frais_remboursables = fields.Float(string='Frais Remboursables' ,default=0.0)
+    prim_rendement = fields.Float(string='Prime de Rendement' ,default=0.0)
+    nombre_jour = fields.Integer(string='Nombre de Jour' ,default=0.0)
